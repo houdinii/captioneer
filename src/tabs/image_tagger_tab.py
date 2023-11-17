bac2926af9a1f148766421b69e8c8e5af3669853
Brian Barnes Jr <houdinii1984@gmail.com>
2023-11-16 19:17:37 -0700

    updated image tagger

--- src/tabs/image_tagger_tab.py
+++ src/tabs/image_tagger_tab.py
@@ -1,5 +1,4 @@
 import os
-
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (
     QWidget,
@@ -16,19 +15,67 @@ from PyQt5.QtWidgets import (
 class ImageTaggerTab(QWidget):
     def __init__(self, app, configuration):
         super().__init__()
-
-        self.module = "image_tagger_tab"
-
         self.app = app
         self.configuration = configuration
+        self.initialize_variables()
+        self.setup_ui()
+        self.connect_signals()
+        self.load_image_text_map()
+        self.display_first_image()
 
+    def initialize_variables(self):
+        self.module = "image_tagger_tab"
         self.image_dir = self.configuration.image_tagger_folder
-
         self.index = 0
         self.text_modified = False
         self.image_text_map = {}
 
-        # Track image/text pairs
+    def setup_ui(self):
+        self.setup_image_text_ui()
+        self.setup_button_ui()
+        self.setup_folder_ui()
+        self.setup_main_layout()
+
+    def setup_image_text_ui(self):
+        self.image_label = QLabel()
+        self.text_edit = QTextEdit()
+        self.image_text_layout = QHBoxLayout()
+        self.image_text_layout.addWidget(self.image_label)
+        self.image_text_layout.addWidget(self.text_edit)
+
+    def setup_button_ui(self):
+        self.prev_button = QPushButton("Previous")
+        self.save_button = QPushButton("Save")
+        self.next_button = QPushButton("Next")
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addWidget(self.prev_button)
+        self.button_layout.addWidget(self.save_button)
+        self.button_layout.addWidget(self.next_button)
+
+    def setup_folder_ui(self):
+        self.folder_text = QLineEdit()
+        self.folder_button = QPushButton("Browse")
+        self.folder_text.setText(self.configuration.image_tagger_folder)
+        self.folder_layout = QHBoxLayout()
+        self.folder_layout.addWidget(self.folder_text)
+        self.folder_layout.addWidget(self.folder_button)
+
+    def setup_main_layout(self):
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.image_text_layout)
+        self.main_layout.addLayout(self.button_layout)
+        self.main_layout.insertLayout(0, self.folder_layout)
+        self.setLayout(self.main_layout)
+
+    # noinspection PyUnresolvedReferences
+    def connect_signals(self):
+        self.prev_button.clicked.connect(self.prev_image)
+        self.next_button.clicked.connect(self.next_image)
+        self.text_edit.textChanged.connect(self.set_text_modified)
+        self.save_button.clicked.connect(self.save_text)
+        self.folder_button.clicked.connect(self.select_folder)
+
+    def load_image_text_map(self):
         if self.image_dir:
             for f in os.listdir(self.image_dir):
                 if f.endswith(".jpg") or f.endswith(".png"):
@@ -36,80 +83,51 @@ class ImageTaggerTab(QWidget):
                     text_file = name + ".txt"
                     self.image_text_map[f] = text_file
 
-        # UI elements
-        self.image_label = QLabel()
-        self.text_edit = QTextEdit()
-
-        self.prev_button = QPushButton("Previous")
-        self.save_button = QPushButton("Save")
-        self.next_button = QPushButton("Next")
-
-        # Layout
-        image_text_layout = QHBoxLayout()
-        image_text_layout.addWidget(self.image_label)
-        image_text_layout.addWidget(self.text_edit)
-
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self.prev_button)
-        button_layout.addWidget(self.save_button)
-        button_layout.addWidget(self.next_button)
-
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(image_text_layout)
-        main_layout.addLayout(button_layout)
-
-        self.setLayout(main_layout)
-
-        # Folder selection
-        self.folder_text = QLineEdit()
-        self.folder_button = QPushButton("Browse")
-
-        self.folder_text.setText(self.configuration.image_tagger_folder)
-
-        folder_layout = QHBoxLayout()
-        folder_layout.addWidget(self.folder_text)
-        folder_layout.addWidget(self.folder_button)
-
-        main_layout.insertLayout(0, folder_layout)
-
-        # Connect signals
-        # noinspection PyUnresolvedReferences
-        self.prev_button.clicked.connect(self.prev_image)
-        # noinspection PyUnresolvedReferences
-        self.next_button.clicked.connect(self.next_image)
-        # noinspection PyUnresolvedReferences
-        self.text_edit.textChanged.connect(self.set_text_modified)
-        # noinspection PyUnresolvedReferences
-        self.save_button.clicked.connect(self.save_text)
-        # noinspection PyUnresolvedReferences
-        self.folder_button.clicked.connect(self.select_folder)
-
-        # Display first image
+    def display_first_image(self):
         self.display_image()
 
     def select_folder(self):
         dialog = QFileDialog()
         start_dir = self.folder_text.text()
-        folder = dialog.getExistingDirectory()
-        self.folder_text.setText(folder)
+        folder = dialog.getExistingDirectory(self, "Select Folder", start_dir)
 
-        self.configuration.folder = folder
-        # self.settings.setValue("folder", folder)
+        if folder:  # Check if a folder was selected
+            self.folder_text.setText(folder)
+            self.configuration.folder = folder
+            print(f"New folder: {self.configuration.folder}")
 
-        print(f"New folder: {self.configuration.folder}")
+            self.image_dir = folder  # Update the image directory
+            self.index = 0  # Reset index to start from the first image
+            self.load_image_text_map()  # Load new images and texts
+            self.display_image()  # Display the first image from the new folder
 
-        self.display_image()
-        self.configuration.save_settings()
-        # self.settings.sync()
+            self.configuration.save_settings()
 
-    def save_text(self, text):
-        pass
+    def set_text_modified(self):
+        self.text_modified = True
+
+    def save_text(self):
+        if not self.text_modified:
+            return  # No changes to save
+
+        if self.index < len(self.image_text_map):
+            filename = sorted(self.image_text_map)[self.index]
+            text_file = self.image_text_map[filename]
+            text_path = os.path.join(self.image_dir, text_file)
+
+            text_to_save = self.text_edit.toPlainText()
+
+            try:
+                with open(text_path, "w") as file:
+                    file.write(text_to_save)
+                print(f"Saved text for {filename}")
+                self.text_modified = False  # Reset the flag after saving
+            except Exception as e:
+                print(f"Error saving text for {filename}: {e}")
 
     def display_image(self):
         text_file = ""
         text = ""
-        # Load image and text
-
         if self.index < len(self.image_text_map):
             filename = sorted(self.image_text_map)[self.index]
             text_file = self.image_text_map[filename]
@@ -124,12 +142,18 @@ class ImageTaggerTab(QWidget):
                 print(e)
 
         self.text_edit.setPlainText(text)
+        self.text_modified = False  # Reset the modification flag
 
     def prev_image(self):
-        pass
+        if len(self.image_text_map) > 0:
+            self.save_text()
+            # Decrement index and wrap around if necessary
+            self.index = (self.index - 1) % len(self.image_text_map)
+            self.display_image()
 
     def next_image(self):
-        pass
-
-    def set_text_modified(self):
-        pass
+        if len(self.image_text_map) > 0:
+            self.save_text()
+            # Increment index and wrap around if necessary
+            self.index = (self.index + 1) % len(self.image_text_map)
+            self.display_image()
